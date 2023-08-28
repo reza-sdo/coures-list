f04089ef009b63ecd08b15f52e1a7a09668861a8
Reza <rezammdzd0@gmail.com>
2023-08-28 14:28:56 +0330

    start

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return <h1>Hi this is my app</h1>;
+}
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App />);
